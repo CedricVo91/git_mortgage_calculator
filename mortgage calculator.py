d92ad9e1f53fc743b568d148490e7dbbded40680
Cedric <cedricvogt91@gmail.com>
2021-07-12 02:00:00 +0200

    first draft 2.0

--- mortgage calculator.py
+++ mortgage calculator.py
@@ -1,2 +1,11 @@
-print("hello world")
-print("hello Git")
+
+
+
+print("Please enter your equity in CHF")
+equity = int(input())
+if equity < 100000:
+    print("Sorry, your equity is too low to obtain a mortgage in Zurich")
+else:
+    if equity >= 100000:
+        mortgage = equity/2
+        print("The offered mortgage will be " + str(mortgage))
