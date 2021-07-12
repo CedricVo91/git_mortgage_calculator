c4af7ee7633ee845e18fcc1d2ec0333d03bacf5f
Cedric <cedricvogt91@gmail.com>
2021-07-12 02:42:53 +0200

    finished draft

--- mortgage calculator.py
+++ mortgage calculator.py
@@ -9,3 +9,8 @@ else:
     if equity >= 100000:
         mortgage = equity/2
         print("The offered mortgage will be " + str(mortgage))
+print("Please enter your desired time period for downpayment first, followed by desired interest rate")
+time = int(input())
+rate = int(input())/100
+monthlypayment = (rate*mortgage*(1+rate)**time)/((1+rate)**time-1)
+print("your monthly payment will be: " + str(int(monthlypayment)))
